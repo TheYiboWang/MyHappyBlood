71f85beef0bd1a593c49693b2503d4256d198a57
rhwang94 <rhwang94@users.noreply.github.com>
2016-05-19 06:56:35 -0700

    Update setupPushReminder.js

--- MHB_Mobile/client/ui/setupPushReminder.js
+++ MHB_Mobile/client/ui/setupPushReminder.js
@@ -1,23 +1,20 @@
-Template.setupPushReminder.events({
 
 'submit form': function(event){
     event.preventDefault();
     var documentId = this._id;
-	var currentTime = new Date(); //Store date as an iso date object
-	var currentUserId = Meteor.userId();
     var playerNameVar = event.target.playerName.value;
+    var notifTime = document.forms[0];
+
+		const notificationData = {
+			timestamp: currentTime,
+			notificationTime: f1[0].value,
+			createdBy: currentUserId
+		};
+
     Meteor.users.update( { _id: currentUserId }, {
 			$push: {
-                    from: 'push',
-                    title: 'Reminder',
-                    text: 'Just a friendly reminder that you need to take your medication!',
-                    badge: badge,
-                    query: {
-                        // Ex. send to a specific user if using accounts:
-                        //userId: 'xxxxxxxxx'
-                    }
+				notifData: notificationData
+			}
 		});
     event.target.playerName.value = "";
 }
-
-})
