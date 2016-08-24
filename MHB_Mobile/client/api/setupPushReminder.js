787eb47b00dad2360e1db506926d870e340f801e
mingh2 <mingh2@uci.edu>
2016-08-23 18:47:58 -0700

    Setup Reminder delete

--- MHB_Mobile/client/api/setupPushReminder.js
+++ MHB_Mobile/client/api/setupPushReminder.js
@@ -10,55 +10,8 @@ Template.setupPushReminder.helpers({
 
 Template.setupPushReminder.events({
 
-	"click #removeHistory": function () {
-		Meteor.call("removeHistory");
+	"click .delete": function () {
+		PastReminders.remove(this._id);
 	},
-	"click input[type=checkbox]": function () {
-		Session.set("checked", $("input[type=checkbox]").is(":checked"));
-	},
-
-	'submit form': function(event){
-    event.preventDefault();
-    var documentId = this._id;
-    var notificationTime =  document.getElementById('n1').value; //get form input
-    console.log(notificationTime);
-    var currentUserId = Meteor.userId();
-
-		const notificationData = {
-			timestamp: notificationTime,
-			//notificationTime: document[0].value,
-			createdBy: currentUserId
-		};
-	Meteor.call("serverNotification");
-    Meteor.users.update( { _id: currentUserId }, {
-			$push: {
-				notifData: notificationData
-			}
-		});
-	}
 
 });
-
-Meteor.startup(function () {
-	Meteor.defer(function () {
-		Session.setDefault("checked", $("input[type=checkbox]").is(":checked"));
-	});
-
-	if (Meteor.isCordova) {
-		window.alert = navigator.notification.alert;
-	}
-
-	Push.addListener('message', function(notification) {
-		// Called on every message
-		console.log(JSON.stringify(notification))
-
-		function alertDismissed() {
-			PastReminders.update({_id: notification.payload.historyId}, {
-				$set: {
-					"receivedAt": new Date()
-				}
-			});
-		}
-		alert(notification.message, alertDismissed, notification.payload.title, "Ok");
-	});
-})
